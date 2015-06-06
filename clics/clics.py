81af99ce29d083068ae470c7d75d19f86852e7c2
Anqur Vanillapy <se14luxingyu@foxmail.com>
2015-06-06 16:39:39 +0800

    New a simplier skeleton for the project and add to a static cursor.

--- clics/clics.py
+++ clics/clics.py
@@ -1,25 +1,88 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os, sys, argparse, webbrowser, time
 from bottle import *
 from jinja2 import *
 
 env = Environment(loader=PackageLoader('clics', 'layouts'))
 template = env.get_template('default.html')
 
+def init_path():
+
+    path = os.getcwd()
+    # Checkout whether in path
+    if path not in sys.path:
+        sys.path.append(path)
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(
+        description='clics: command-line interface chat shell',
+        add_help=False,
+        )
+
+    parser.add_argument(
+        '-h', '--hostname',
+        dest='hostname',
+        action='store',
+        default='localhost',
+        )
+
+    parser.add_argument(
+        '-p', '--port',
+        dest='port',
+        action='store',
+        type=int,
+        default=8080,
+        )
+
+    parser.add_argument(
+        '-u', '--username',
+        dest='username',
+        action='store',
+        default='',
+        )
+
+    parser.add_argument(
+        '-d', '--debug',
+        dest='debug',
+        action='store_true',
+        default=False,
+        )
+
+    return parser.parse_args()
+
+def init_browser(hostname, port, username):
+
+    url = "http://%s:%d/%s" % (hostname, port, username)
+    # print url
+    webbrowser.open(url)
 
 @route('/')
 @route('/<username>')
-def greet(username='ranger'):
+def app_main(username=''):
 
-    help_msg = True
-    if username != 'ranger':
-        help_msg = False
-    return template.render(username=username, help_msg=help_msg)
+    run_main = True
+    cursor = ' '
+
+    if username == '':
+        username = 'ranger'
+        run_main = False
+
+    return template.render(
+        username=username,
+        run_main=run_main,
+        cursor=cursor,
+        )
 
 @route('/layouts/<filename:path>')
 def send_static(filename):
-    
+
     return static_file(filename, root='./layouts', download=True)
 
-run(host='localhost', port=29, debug=True)
+if __name__ == '__main__':
+    init_path()
+    args = parse_arguments()
+    init_browser(args.hostname, args.port, args.username)
+    run(host=args.hostname, port=args.port, debug=args.debug)
